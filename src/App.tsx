dc4997eac19e2e168d57fc1435cc12063bc9a886
Yernar <yernar.mussin@nitka.com>
2021-07-28 14:03:39 +0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.sass';
+import AppAside from './components/Aside';
+import playLogo from './img/play.png';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+export interface VideoI {
+  id: number;
+  time: number;
+}
 
 function App() {
+
+  const [categories, setCategories] = useState([]);
+  const [minutes, setMinutes] = useState<VideoI[]>([]);
+  const [blocks, setBlocks] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://rpback.com/api/games/test_categories?project_id=2').then(({ data }) => {
+      setCategories(data.categories);
+    })
+
+    axios.get('https://rpback.com/api/games/test_minutes?project_id=2').then(({ data }) => {
+      setMinutes(data.minutes);
+    })
+
+    axios.get('https://rpback.com/api/games/test_blocks?project_id=2').then(({ data }) => {
+      setBlocks(data.blocks);
+      console.log(blocks);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <AppAside/>
+        <main className="app-main">
+          <div className="app-main__head">
+            <h1>Courses</h1>
+
+            <div className="app-main__navigation">
+              {categories.map((category) => <span className="app-main__navigation__item">{category}</span>)}
+            </div>
+          </div>
+
+          <div className="app-main__content">
+            {minutes.map((minute) =>
+                <div className="app-main__content__video" key={minute.id}>
+                  <div className="app-main__content__video__title">Illustration</div>
+                  <div className="app-main__content__video__sub">24 lessons</div>
+                  <img src={playLogo} className="app-main__content__video__play-logo" alt="play" />
+                  <div className="app-main__content__video__duration">{minute.time} min</div>
+                </div>
+            )}
+
+            <div className="app-main__content__video app-main__content__video_building">
+              <div className="app-main__content__video__title">Graphic design</div>
+              <div className="app-main__content__video__sub">30 lessons</div>
+              <img src={playLogo} className="app-main__content__video__play-logo" alt="play" />
+              <div className="app-main__content__video__duration">236 min</div>
+            </div>
+          </div>
+        </main>
     </div>
   );
 }
---
+++ src/components/Aside.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {ReactComponent as DashboardLogo} from './../img/dashboard.svg';
+import {ReactComponent as ChecksLogo} from './../img/checks.svg';
+import {ReactComponent as CustomerLogo} from './../img/customer.svg';
+
+function AppAside() {
+  return (
+    <aside className="app-aside">
+      <div className="app-aside__nav app-aside__nav_active">
+        <DashboardLogo />
+      </div>
+      <div className="app-aside__nav">
+        <ChecksLogo />
+      </div>
+      <div className="app-aside__nav">
+        <CustomerLogo />
+      </div>
+    </aside>
+
+  );
+}
+
+export default AppAside;
